82087cfb0350d6edba4dc105289486ce5b33829b
Firdavs Gafurjonov <firdavs.gafurjonov@gmail.com>
2022-02-10 00:05:04 +0900

    completed!

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import { Provider } from 'react-redux';
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
